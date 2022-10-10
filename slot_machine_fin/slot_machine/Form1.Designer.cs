ff5b032c0af1b51b7d133b86d722f5e3c4de40c1
DEAD POOL <78198419+not-DEADPOOL@users.noreply.github.com>
2022-10-10 21:29:38 +0200

    Add files via upload

---
+++ slot_machine_fin/slot_machine/Form1.Designer.cs
@@ -0,0 +1,124 @@
+﻿
+namespace slot_machine
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.imgslot = new System.Windows.Forms.PictureBox();
+            this.img1 = new System.Windows.Forms.PictureBox();
+            this.img2 = new System.Windows.Forms.PictureBox();
+            this.img3 = new System.Windows.Forms.PictureBox();
+            this.btnleva = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.imgslot)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.img1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.img2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.img3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // imgslot
+            // 
+            this.imgslot.Image = ((System.Drawing.Image)(resources.GetObject("imgslot.Image")));
+            this.imgslot.Location = new System.Drawing.Point(-117, -26);
+            this.imgslot.Name = "imgslot";
+            this.imgslot.Size = new System.Drawing.Size(1020, 500);
+            this.imgslot.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.imgslot.TabIndex = 0;
+            this.imgslot.TabStop = false;
+            // 
+            // img1
+            // 
+            this.img1.Image = ((System.Drawing.Image)(resources.GetObject("img1.Image")));
+            this.img1.Location = new System.Drawing.Point(139, 139);
+            this.img1.Name = "img1";
+            this.img1.Size = new System.Drawing.Size(116, 155);
+            this.img1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.img1.TabIndex = 1;
+            this.img1.TabStop = false;
+            // 
+            // img2
+            // 
+            this.img2.Image = ((System.Drawing.Image)(resources.GetObject("img2.Image")));
+            this.img2.Location = new System.Drawing.Point(298, 139);
+            this.img2.Name = "img2";
+            this.img2.Size = new System.Drawing.Size(127, 162);
+            this.img2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.img2.TabIndex = 2;
+            this.img2.TabStop = false;
+            // 
+            // img3
+            // 
+            this.img3.Image = ((System.Drawing.Image)(resources.GetObject("img3.Image")));
+            this.img3.Location = new System.Drawing.Point(463, 139);
+            this.img3.Name = "img3";
+            this.img3.Size = new System.Drawing.Size(121, 155);
+            this.img3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.img3.TabIndex = 3;
+            this.img3.TabStop = false;
+            // 
+            // btnleva
+            // 
+            this.btnleva.BackColor = System.Drawing.Color.Transparent;
+            this.btnleva.Location = new System.Drawing.Point(728, 74);
+            this.btnleva.Name = "btnleva";
+            this.btnleva.Size = new System.Drawing.Size(60, 47);
+            this.btnleva.TabIndex = 4;
+            this.btnleva.Text = "Gioca!";
+            this.btnleva.UseVisualStyleBackColor = false;
+            this.btnleva.Click += new System.EventHandler(this.btnleva_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnleva);
+            this.Controls.Add(this.img3);
+            this.Controls.Add(this.img2);
+            this.Controls.Add(this.img1);
+            this.Controls.Add(this.imgslot);
+            this.Name = "Form1";
+            this.Text = "Slot Machine!1!!1";
+            ((System.ComponentModel.ISupportInitialize)(this.imgslot)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.img1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.img2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.img3)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox imgslot;
+        private System.Windows.Forms.PictureBox img1;
+        private System.Windows.Forms.PictureBox img2;
+        private System.Windows.Forms.PictureBox img3;
+        private System.Windows.Forms.Button btnleva;
+    }
+}
+
---
+++ slot_machine_fin/slot_machine/Form1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace slot_machine
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnleva_Click(object sender, EventArgs e)
+        {
+            Random rnd = new Random();
+            // ciliegia=1
+            //uva=3
+            //mela=2
+            int img_1 = 0, img_2 = 0, img_3 = 0;
+            img1.Image = Image.FromFile("7.png");
+            img2.Image = Image.FromFile("7.png");
+            img3.Image = Image.FromFile("7.png");
+            switch (rnd.Next(1,4))
+            {
+                case 1:
+                    img1.Image = Image.FromFile("ciliegie.jpg");
+                    img_1 = 1;
+                    break;
+                case 2: 
+                    img1.Image = Image.FromFile("mela.jpg");
+                    img_1 = 2;
+                    break;
+                case 3:
+                    img1.Image = Image.FromFile("uva.jpg");
+                    img_1 = 3;
+                    break;
+                default:
+                    break;
+            }
+            switch (rnd.Next(1, 4))
+            {
+                case 1:
+                    img2.Image = Image.FromFile("ciliegie.jpg");
+                    img_2 = 1;
+                    break;
+                case 2:
+                    img2.Image = Image.FromFile("mela.jpg");
+                    img_2 = 2;
+                    break;
+                case 3:
+                    img2.Image = Image.FromFile("uva.jpg");
+                    img_2 = 3;
+                    break;
+                default:
+                    break;
+            }
+            switch (rnd.Next(1, 4))
+            {
+                case 1:
+                    img3.Image = Image.FromFile("ciliegie.jpg");
+                    img_3 = 1;
+                    break;
+                case 2:
+                    img3.Image = Image.FromFile("mela.jpg");
+                    img_3 = 2;
+                    break;
+                case 3:
+                    img3.Image = Image.FromFile("uva.jpg");
+                    img_3 = 3;
+                    break;
+                default:
+                    break;
+            }
+
+            if (img_1 == 1 && img_2 == 1 && img_3 == 1)
+                MessageBox.Show("Hai Vinto!!");
+
+            else if (img_1 == 2 && img_2 == 2 && img_3 == 2)
+                MessageBox.Show("Hai Vinto!!");
+
+            else if (img_1 == 3 && img_2 == 3 && img_3 == 3)
+                MessageBox.Show("Hai Vinto!!");
+
+            else
+                MessageBox.Show("Riprova!");
+        }
+    }
+}
